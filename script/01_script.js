44f0cbfeaf9adba8e3cdf14e0f4bdd534a2fdd45
Hasibullah Aman <hasib.aman7@gmail.com>
2023-01-04 00:41:49 +0430

    03

--- script/01_script.js
+++ script/01_script.js
@@ -1,1 +1,20 @@
-$("h1").css("color", "green");
+// $("h1").css("font-size","5rem")
+// $("h1").addClass("mystyl");
+
+// $("h1").text("<em> Hasibullah Aman </em>");
+// $("h1").html("<em>")
+// $("a").attr("href","https://www.instagram.com");
+
+
+// $("button").click(function(){
+//     $("h1").css("color","green")
+// })
+
+
+// $(document).keypress(function(event){
+//     $("h1").text(event.key);
+// })
+
+$("button").on("click",function(){
+    $("h1").css("color","teal")
+})
